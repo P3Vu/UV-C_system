78ced7d7cdba534ce398eb35a8b9ff964cbef50d
P3Vu <p3wojciechowski@gmail.com>
2020-11-26 19:32:56 +0100

    Dodanie obslugi 4x Switch Relay-a

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,40 +1,88 @@
-/* Hello World Example
+/**
+  ******************************************************************************
+  * @file       main.c
+  * @author     Pawel Wojciechowski
+  ******************************************************************************
+  */
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
 
+
+
+#define LAMP1 GPIO_NUM_2
+#define LAMP2 GPIO_NUM_0
+#define LAMP3 GPIO_NUM_18
+#define LAMP4 GPIO_NUM_19
+
+#define ON      0
+#define OFF     1
+
+static const char* TAG = "Module";
+
+/** Configure GPIO's for relay switch */
+static void configGPIO(void){
+
+    int err = 0;
+
+    err = gpio_set_direction(LAMP1, GPIO_MODE_OUTPUT);
+    if(err != ESP_OK) {
+            ESP_LOGW(TAG, "Fail to configure Switch Relay GPIO's at GPIO : %d", LAMP1);
+            return;
+    }
+    gpio_set_level(LAMP1, OFF);
+
+    err = gpio_set_direction(LAMP2, GPIO_MODE_OUTPUT);
+    if(err != ESP_OK){
+        ESP_LOGW(TAG, "Fail to configure Switch Relay GPIO's at GPIO : %d", LAMP2);
+        return;
+    }
+    gpio_set_level(LAMP2, OFF);
+
+    err = gpio_set_direction(LAMP3, GPIO_MODE_OUTPUT);
+    if(err != ESP_OK){
+        ESP_LOGW(TAG, "Fail to configure Switch Relay GPIO's at GPIO : %d", LAMP3);
+        return;
+    }
+    gpio_set_level(LAMP3, OFF);
+
+    err = gpio_set_direction(LAMP4, GPIO_MODE_OUTPUT);
+    if(err != ESP_OK){
+        ESP_LOGW(TAG, "Fail to configure Switch Relay GPIO's at GPIO : %d", LAMP4);
+        return;
+    }
+    gpio_set_level(LAMP4, OFF);
+
+    ESP_LOGI(TAG, "Switch Relay GPIO's config success.");
+    return;
+}
+/** Turn On the Lamp */
+static void TurnOnLamp(int gpioNum){
+    int err = 0;
+    err = gpio_set_level(gpioNum, ON);
+    if(err != ESP_OK) ESP_LOGW(TAG, "Fail to set Lamp On at GPIO: %d", gpioNum);
+
+    return;
+}
+
+/** Turn Off the Lamp */
+static void TurnOffLamp(int gpioNum){
+    int err = 0;
+    err = gpio_set_level(gpioNum, OFF);
+    if(err != ESP_OK) ESP_LOGW(TAG, "Fail to set Lamp Off at GPIO: %d", gpioNum);
+
+    return;
+}
 
 void app_main()
 {
-    printf("Hello world!\n");
+    configGPIO();
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
+
+
