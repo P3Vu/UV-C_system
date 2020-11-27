be42290633e0b439cfb425c9161f587ffe93fc61
P3Vu <p3wojciechowski@gmail.com>
2020-11-27 20:33:43 +0100

    Implementacja mikrofalowego czujnika ruchu z wykorzystaniem przerwania

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "esp_log.h"
@@ -15,51 +16,82 @@
 
 
 
-#define LAMP1 GPIO_NUM_2
-#define LAMP2 GPIO_NUM_0
-#define LAMP3 GPIO_NUM_18
-#define LAMP4 GPIO_NUM_19
+#define LAMP1           GPIO_NUM_2                    /* RELAY IN1 on board */
+#define LAMP2           GPIO_NUM_0                    /* RELAY IN2 on board */
+#define LAMP3           GPIO_NUM_18                   /* RELAY IN3 on board */
+#define LAMP4           GPIO_NUM_19                   /* RELAY IN4 on board */
+#define GPIO_OUTPUT_PIN_SEL ((1ULL<<GPIO_NUM_2) | (1ULL<<GPIO_NUM_0) | (1ULL<<GPIO_NUM_18) | (1ULL<<GPIO_NUM_19))
+
+#define MOTION_SENSOR   GPIO_NUM_13               /* Microwave Motion Sensor */
+#define GPIO_INPUT_PIN_SEL (1ULL<<MOTION_SENSOR)
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static xQueueHandle gpio_evt_queue = NULL;
 
 #define ON      0
 #define OFF     1
 
 static const char* TAG = "Module";
 
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
+
 /** Configure GPIO's for relay switch */
 static void configGPIO(void){
 
-    int err = 0;
+    gpio_config_t io_conf;
 
-    err = gpio_set_direction(LAMP1, GPIO_MODE_OUTPUT);
-    if(err != ESP_OK) {
-            ESP_LOGW(TAG, "Fail to configure Switch Relay GPIO's at GPIO : %d", LAMP1);
-            return;
-    }
-    gpio_set_level(LAMP1, OFF);
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that want to set
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
 
-    err = gpio_set_direction(LAMP2, GPIO_MODE_OUTPUT);
-    if(err != ESP_OK){
-        ESP_LOGW(TAG, "Fail to configure Switch Relay GPIO's at GPIO : %d", LAMP2);
-        return;
-    }
-    gpio_set_level(LAMP2, OFF);
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    //bit mask of the pins that will be inputs
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
 
-    err = gpio_set_direction(LAMP3, GPIO_MODE_OUTPUT);
-    if(err != ESP_OK){
-        ESP_LOGW(TAG, "Fail to configure Switch Relay GPIO's at GPIO : %d", LAMP3);
-        return;
-    }
-    gpio_set_level(LAMP3, OFF);
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "Motion_Task", 2048, NULL, 10, NULL);
 
-    err = gpio_set_direction(LAMP4, GPIO_MODE_OUTPUT);
-    if(err != ESP_OK){
-        ESP_LOGW(TAG, "Fail to configure Switch Relay GPIO's at GPIO : %d", LAMP4);
-        return;
-    }
-    gpio_set_level(LAMP4, OFF);
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(MOTION_SENSOR, gpio_isr_handler, (void*) MOTION_SENSOR);
+    // remove isr handler for gpio number
+    gpio_isr_handler_remove(MOTION_SENSOR);
+    // hook isr handler for specific gpio pin again
+    gpio_isr_handler_add(MOTION_SENSOR, gpio_isr_handler, (void*) MOTION_SENSOR);
 
-    ESP_LOGI(TAG, "Switch Relay GPIO's config success.");
-    return;
+    ESP_LOGI(TAG, "GPIO Config finished.");
 }
 /** Turn On the Lamp */
 static void TurnOnLamp(int gpioNum){
@@ -82,6 +114,12 @@ static void TurnOffLamp(int gpioNum){
 void app_main()
 {
     configGPIO();
+
+    int cnt = 0;
+    while(1) {
+        printf("cnt: %d\n", cnt++);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
 
 }
 
